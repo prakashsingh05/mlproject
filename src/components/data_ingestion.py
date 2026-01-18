cac72cf6b557d0e6c0be9041c0a5000481afbb6b
prakashsingh05 <prakash26singh2@gmail.com>
2026-01-18 20:35:02 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from src.logger import logging
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig  
+
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
 
@@ -64,4 +67,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data) 
+    train_arr,test_arr,*_ = data_transformation.initiate_data_transformation(train_data, test_data) 
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,9 @@
 import os
-import sys 
-import numpy as np
-import pandas as pd
+import sys
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
-
 
 
 def save_object(file_path, obj):
@@ -15,6 +13,33 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Train and evaluate multiple ML models
+    """
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            # Train model
+            model.fit(X_train, y_train)
+
+            # Predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
